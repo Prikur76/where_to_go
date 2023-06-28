f479f418a5db211c71bf59e21888f739ae76e8e6
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-06-28 11:12:03 +0300

    Add Sight model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Sight
+
 
 # Register your models here.
+@admin.register(Sight)
+class SightAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title',
+                'description_short',
+                'description_long',
+                ('longtitude', 'latitude'),
+                'imgs')
+        }),
+    )
--- places/models.py
+++ places/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Sight(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='название')
+    description_short = models.TextField(
+        verbose_name='краткое описание')
+    description_long = models.TextField(
+        verbose_name='описание')
+    longtitude = models.FloatField(verbose_name='Долгота')
+    latitude = models.FloatField(verbose_name='Широта')
+    imgs = models.ImageField(
+        upload_to='images/',
+        verbose_name='Фото', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'достопримечательность'
+        verbose_name_plural = 'достопримечательности'
+    def __str__(self):
+        return self.title
