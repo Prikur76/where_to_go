20fd6a859aefdc2da1bd200410be7640050b1fa5
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-06-28 09:41:57 +0300

    Renamed app

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/index.html')
+    return render(request, 'app/index.html')
