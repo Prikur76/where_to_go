ca445fe1314d346a35a96b5e1b191426678e3b51
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-06-28 14:20:22 +0300

    Fix .gitignore

--- places/models.py
+++ places/models.py
@@ -12,12 +12,38 @@ class Sight(models.Model):
         verbose_name='описание')
     longtitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
-    imgs = models.ImageField(
-        upload_to='images/',
-        verbose_name='Фото', blank=True, null=True)
 
     class Meta:
-        verbose_name = 'достопримечательность'
-        verbose_name_plural = 'достопримечательности'
+        verbose_name = 'экскурсия'
+        verbose_name_plural = 'экскурсии'
+
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    def image_directory_path(self, filename):
+        return 'images/{0}/{1}{2}'.format(
+            self.sight.id,
+            self.order_id,
+            filename)
+
+    sight = models.ForeignKey(
+        Sight,
+        on_delete=models.CASCADE,
+        verbose_name='место',
+        related_name='images'
+    )
+    order_id = models.PositiveIntegerField(
+        verbose_name='порядок'
+    )
+    image = models.ImageField(
+        upload_to=image_directory_path,
+        verbose_name='фото'
+        )
+    class Meta:
+        verbose_name = 'фото'
+        verbose_name_plural = 'фото'
+
+    def __str__(self):
+        return f'{self.order_id} {self.sight.title}'
