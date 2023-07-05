7a9569b9857b5358c4edba0e26bf776b908d8ec4
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-07-05 11:52:55 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,7 +1,53 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Sight
 
-
-# Create your views here.
 
 def index(request):
-    return render(request, 'places/index.html')
+    places = Sight.objects.all()
+    places_details = []
+    for place in places:
+        places_details.append(
+          {
+            'title': place.title,
+            'imgs': [('media/' + i) for i in place.images.all().values_list('upload', flat=True)],
+            'description_short': place.description_short,
+            'description_long': place.description_long,
+            'coordinates': {
+                'lat': place.latitude,
+                'lng': place.longtitude,
+            }
+          }
+        )
+    context = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "./places/static/moscow_legends.json"
+          },
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "./places/static/roofs24.json"
+          }
+        }
+      ]
+    }
+
+    return render(request,
+                  'places/index.html',
+                  context={'value': context})
