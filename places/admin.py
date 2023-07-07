b6d90951e376f51c9027d42aced8c18f02ae475c
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-07-07 11:31:54 +0300

    Add preview to admin models and inlines

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 0
+    fields = ['upload', 'get_preview', 'order_id']
+    readonly_fields = ['get_preview']
+
+    def get_preview(self, obj):
+        if obj.upload:
+            return mark_safe('<img src="%s" width="100"/>' % obj.upload.url)
+
+    get_preview.short_description = 'миниатюра'
 
 
 @admin.register(Place)
@@ -29,3 +38,10 @@ class PlaceAdmin(admin.ModelAdmin):
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_filter = ('place__place_en', )
+    readonly_fields = ['preview',]
+
+    def preview(self, obj):
+        if obj.upload:
+            return mark_safe('<img src="%s" width="200"/>' % obj.upload.url)
+
+    preview.short_description = 'миниатюра'
