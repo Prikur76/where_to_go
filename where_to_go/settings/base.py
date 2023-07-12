cf6c3c305bfaf89484a336c987f0a7a7d26a4db9
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-07-12 17:18:17 +0300

    Fix errors

--- where_to_go/settings/base.py
+++ where_to_go/settings/base.py
@@ -98,7 +98,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'places/static')
-STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
