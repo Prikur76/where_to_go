a4b81c85f9e86631a95f0284ba1fc83cb439e1f7
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-08-08 12:15:42 +0300

    Fix errors in load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,8 +1,11 @@
 import os
+import urllib
 
+import requests
 from django.core.management.base import BaseCommand
+from places.get_images import download_image_from_url
 from places.models import Place, Image
-from places.services import download_image_from_url, get_place_from_link
+from slugify import slugify
 
 
 class Command(BaseCommand):
@@ -16,14 +19,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         link = kwargs['link']
-        place = get_place_from_link(link)
+        response = requests.get(link)
+        response.raise_for_status()
+        place = response.json()
         created_place, status = Place.objects.get_or_create(
             title=place['title'],
-            slug=place['slug'],
-            description_short=place['description_short'],
-            description_long=place['description_long'],
-            latitude=place['coordinates']['lat'],
-            longtitude=place['coordinates']['lng']
+            defaults={
+                'slug': slugify(place['title']),
+                'description_short': place['description_short'],
+                'description_long': place['description_long'],
+                'latitude': place['coordinates']['lat'],
+                'longtitude': place['coordinates']['lng']
+            }
         )
         if status:
             self.stdout.write(
@@ -34,14 +41,19 @@ class Command(BaseCommand):
             )
 
         for num, img_url in enumerate(place['imgs'], start=1):
-            image_name = download_image_from_url(
+            image_name = os.path.basename(
+                urllib.parse.urlsplit(img_url).path)
+            download_image_from_url(
                 img_url,
-                os.path.join('media', created_place.slug)
+                os.path.join('media', created_place.slug),
+                image_name
             )
             created_image, img_status = Image.objects.get_or_create(
-                place=created_place,
-                order_id=num,
-                upload=os.path.join(created_place.slug, image_name)
+                photo=os.path.join(created_place.slug, image_name),
+                defaults={
+                    'place': created_place,
+                    'order': num,
+                }
             )
             if img_status:
                 self.stdout.write(
